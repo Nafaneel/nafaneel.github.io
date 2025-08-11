9aea2d3e9af630353f242156d44366e2d99c2878
N4F4N33L <nathan.cherry10@gmail.com>
2025-08-11 21:22:35 +0100

    Update authenticate.js

--- services/authenticate.js
+++ services/authenticate.js
@@ -1,1 +1,8 @@
-console.log("FETCHED SUCCESSFULLY");
+var proceedNormalLoad = true
+
+//make relivant checks
+if(proceedNormalLoad){
+    document.getElementById("mainContentJSload").style.visibility = "visible";
+} else {
+    document.write("This Website Is No Longer Valid, Please contact system administrator.")
+}
